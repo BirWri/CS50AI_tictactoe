7e91347c7b2eab679e93c8957e8966aff21c11d5
Birgit Wright <wrightbirgit@gmail.com>
2025-08-11 20:06:11 +0200

    version 1 of how to evaluate board for utility function

--- tictactoe.py
+++ tictactoe.py
@@ -16,9 +16,14 @@ def initial_state():
     """
     Returns starting state of the board.
     """
-    return [[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY]]
+
+    return [[EMPTY, 'X', 'O'],
+            ['O', 'X', EMPTY],
+            ['X', EMPTY, 'O']]
+
+    #return [[EMPTY, EMPTY, EMPTY],
+            #[EMPTY, EMPTY, EMPTY],
+            #[EMPTY, EMPTY, EMPTY]]
 
 
 def player(board):
@@ -113,12 +118,53 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-
+    import numpy as np
+    
     # I guess here the adjesiant pieces are evaluated...?
     # Fetch the user
     # Calculate the sum of rows, columns and the 2 diaogonals
     # where X = 1, O = -1 and None = 0
     # Return 1, -1 or 0
+    numericalBoard = initial_state()
+
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == None:
+                numericalBoard[i][j] = 0
+            if board[i][j] == 'X':
+                numericalBoard[i][j] = 1
+            if board[i][j] == 'O':
+                numericalBoard[i][j] = -1
+    
+    print(numericalBoard)
+    # sum rows
+    list_rows = [sum(i) for i in numericalBoard]
+    if list_rows.count(3) > 0:
+        print("WINNERS")
+    rows =sum(list_rows)
+    print(list_rows)
+    print(f"rows:{rows}")
+
+    # sum columns
+    a = np.array(numericalBoard) # Converting `a` to numpy array
+    list_columns = np.sum(a, axis=0).tolist()
+    if list_columns.count(3) > 0:
+        print("WINNERS")
+    columns = sum(list_columns)
+    print(list_columns)
+    print(f"colums:{columns}")
+    # sum diaginals
+    diag1 = numericalBoard[0][0] + numericalBoard[1][1] + numericalBoard[2][2]
+    diag2 = numericalBoard[0][2] + numericalBoard[1][1] + numericalBoard[2][0]
+    if diag1 == 3 or diag1 == -3 or diag2 == 3 or diag2 == -3:
+        print("WINNERS")
+    diags = diag1+diag2
+    print(f"digs:{diags}")
+    # sum all
+    all = rows+columns+diags
+    print(all)
+    raise NotImplementedError
+
     
 
 def minimax(board):
@@ -126,16 +172,20 @@ def minimax(board):
     Returns the optimal action for the current player on the board.
     """
 
-    # Fetch the user
+    # Fetch the user 
+    # fetch avaiable sets and add to frontier?
+    # Select a node to explore
     # Calculate the sum of rows, columns and the 2 diaogonals
     # where X = 1, O = -1 and None = 0
-    # I think utility is used here to help to determine when selection for action is used
+    # Different statuses will have differents sums...
     # The target is defined by the selection of the player - player O aims for -1 or 0 result,
     # player X aims for 1 or 0 result.
+
 
     availableOption = actions(board)
     print(availableOption)
     move = list(availableOption)[0]
+    utility(board)
 
     return move
     raise NotImplementedError
